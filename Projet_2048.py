0dd96ce0da0a9e6ecb39e1b19bbdbc8d8f94accd
Maéva DERREVEAU <maeva.derreveau@ens.uvsq.fr>
2022-04-15 09:21:19 +0200

    TDHC

--- Projet_2048.py
+++ Projet_2048.py
@@ -1,1 +1,12 @@
 #Projet_2048#
+
+import tkinter as tk
+import random as rd
+
+LARGEUR = 1000
+HAUTEUR = 1000
+
+racine = tk.Tk()
+canvas = tk.Canvas(bg="black", width=1000, height=1000)
+
+racine.mainloop()
