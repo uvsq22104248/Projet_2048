0c7498a7c7c598c908574bcbbac4e6002133b2a2
Maéva DERREVEAU <maeva.derreveau@ens.uvsq.fr>
2022-05-02 12:34:16 +0200

    avancé

--- Projet_2048.py
+++ Projet_2048.py
@@ -3,10 +3,20 @@
 import tkinter as tk
 import random as rd
 
-LARGEUR = 1000
-HAUTEUR = 1000
+HAUTEUR = 700
+LARGEUR = 700
 
 racine = tk.Tk()
-canvas = tk.Canvas(bg="black", width=1000, height=1000)
+canvas = tk.Canvas(racine, bg = "grey", height = HAUTEUR, width = LARGEUR)
 
+button_play = tk.Button(racine, text = "Play", bg = "orange", height = 2, width = 10)
+button_play.grid(row = 0, column = 0)
+
+for i in range(0,4) :
+    canvas.create_line((i*175, 0), (i*175, HAUTEUR), fill="black", width=5)
+    canvas.create_line((0, i*175), (LARGEUR, i*175), fill="black", width=5)
+
+
+
+canvas.grid()
 racine.mainloop()
